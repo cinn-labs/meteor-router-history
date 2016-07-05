5d0b96b87648df30d6546a8656cc1c9fbebca6a3
Christian Storch <ccstorch@gmail.com>
2016-07-05 14:57:42 +0100

    0.0.2

--- package.js
+++ package.js
@@ -1,13 +1,13 @@
 Package.describe({
   name: 'cinn:router-history',
-  version: '0.0.1',
+  version: '0.0.2',
   summary: 'Keep track of FlowRouter navigation history',
   git: 'https://github.com/cinn-labs/meteor-router-history',
   documentation: 'README.md'
 });
 
 Package.onUse(function(api) {
-  api.versionsFrom('1.3.3.1');
+  api.versionsFrom('1.3.4');
   api.use('ecmascript');
   api.use('kadira:flow-router@2.12.1');
   api.export('RouterHistory');
